a05f5c3e3d3bdeba74e166bcedc070bab1f562d9
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-09-07 17:37:20 -0300

    created navbar components, configured environments and routes

---
+++ src/app/components/cadastro-contas/cadastro-contas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-contas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-contas.component.html',
+  styleUrl: './cadastro-contas.component.css'
+})
+export class CadastroContasComponent {
+
+}
---
+++ src/app/components/consulta-contas/consulta-contas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-contas',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulta-contas.component.html',
+  styleUrl: './consulta-contas.component.css'
+})
+export class ConsultaContasComponent {
+
+}
